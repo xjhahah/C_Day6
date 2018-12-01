ae555ceacc6f1e373dc92edff6733bae0a73dab3
xjhahah <908234867@qq.com>
2018-12-01 22:04:48 +0800

    提交代码

--- C_Day6/mian.c
+++ C_Day6/mian.c
@@ -29,60 +29,75 @@
 
 //2.获取一个数二进制序列中所有的偶数位和奇数位，分别输出二进制序列。 
 
-void Test2(int n)
-{
-	printf("奇数位值为：");
-	for (int i = 30; i >= 0; i -= 2)
-	{
-		printf("%d ", (n >> i) & 1);
-	}
-	printf("\n");
+//void Test2(int n)
+//{
+//	printf("奇数位值为：");
+//	for (int i = 30; i >= 0; i -= 2)
+//	{
+//		printf("%d ", (n >> i) & 1);
+//	}
+//	printf("\n");
+//
+//	printf("偶数位值为：");
+//	for (int i = 31; i >= 0; i -= 2)
+//	{
+//		printf("%d ",(n >> i)&1);
+//	}
+//	printf("\n");
+//}
+//
+//int main()
+//{
+//	size_t n = 0;
+//	printf("请输入你要查询的数：");
+//	scanf_s("%d", &n);
+//	//printf("该数中比特位有 %d 个是 1\n",count_one_bits(n));
+//
+//	Test2(n);
+//
+//	//法二
+//	int i = 0, j = 0;
+//	char arr1[16];  //定义2个数组来存放二进制序列
+//	char arr2[16];
+//	int num = 0;
+//	printf("请输入你要查询的数：");
+//	scanf_s("%d", &num);
+//	for (i = 0; i < 32; i += 2)
+//	{
+//		arr1[j] = ((num >> i) & 1);  //奇数位
+//		j++;
+//	}
+//	for (i = 1, j = 0; i < 32; i += 2)
+//	{
+//		arr2[j] = ((num >> i) & 1);  //偶数位
+//		j++;
+//	}
+//	printf("奇数位为：");  //输出数组里的二进制奇数序列
+//	for (i = 0; i <= 15; i++)
+//	{
+//		printf("%d ", arr1[i]);
+//	}
+//	printf("\n");
+//	printf("偶数位为：");  //输出数组里的二进制偶数序列
+//	for (i = 0; i <= 15; i++)
+//	{ 
+//		printf("%d ", arr2[i]);
+//	}
+//	return 0;
+//}
+//  
 
-	printf("偶数位值为：");
-	for (int i = 31; i >= 0; i -= 2)
-	{
-		printf("%d ",(n >> i)&1);
-	}
-	printf("\n");
-}
-
+//输出一个整数的每一位。
 int main()
 {
-	size_t n = 0;
-	printf("请输入你要查询的数：");
-	scanf_s("%d", &n);
-	//printf("该数中比特位有 %d 个是 1\n",count_one_bits(n));
-
-	Test2(n);
-
-	//法二
-	int i = 0, j = 0;
-	char arr1[16];  //定义2个数组来存放二进制序列
-	char arr2[16];
-	int num = 0;
+	int num= 0;
+	int i = 1;
 	printf("请输入你要查询的数：");
 	scanf_s("%d", &num);
-	for (i = 0; i < 32; i += 2)
+	while (i <= 32)
 	{
-		arr1[j] = ((num >> i) & 1);  //奇数位
-		j++;
-	}
-	for (i = 1, j = 0; i < 32; i += 2)
-	{
-		arr2[j] = ((num >> i) & 1);  //偶数位
-		j++;
-	}
-	printf("奇数位为：");  //输出数组里的二进制奇数序列
-	for (i = 0; i <= 15; i++)
-	{
-		printf("%d ", arr1[i]);
-	}
-	printf("\n");
-	printf("偶数位为：");  //输出数组里的二进制偶数序列
-	for (i = 0; i <= 15; i++)
-	{ 
-		printf("%d ", arr2[i]);
+		printf("%d ", (num >> i) & 1);
+		i++;
 	}
 	return 0;
 }
-  
